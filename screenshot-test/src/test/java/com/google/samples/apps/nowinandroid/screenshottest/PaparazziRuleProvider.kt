ab2f489028c4b831c41a6ec3b20764e6c8d565ad
Roman Kamyshnikov <rkam88@gmail.com>
2023-08-06 21:49:31 +0100

    Add sample test

---
+++ screenshot-test/src/test/java/com/google/samples/apps/nowinandroid/screenshottest/PaparazziRuleProvider.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.screenshottest
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams.RenderingMode
+import com.android.ide.common.rendering.api.SessionParams.RenderingMode.SHRINK
+
+object PaparazziRuleProvider {
+    fun get(
+        deviceConfig: DeviceConfig = DeviceConfig.PIXEL_5,
+        renderingMode: RenderingMode = SHRINK,
+    ) = Paparazzi(
+        deviceConfig = deviceConfig,
+        renderingMode = renderingMode,
+        maxPercentDifference = 0.01,
+        showSystemUi = false,
+    )
+}
